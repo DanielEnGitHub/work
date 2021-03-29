e6e8dcaedb32f6eefcaf186e3a64d586219b79d3
Danie Gallina <danieljgallina@gmail.com>
2021-03-29 15:47:13 -0600

    Modificado los modelos y se agrego la direccion para los templates

--- apps/adopcion/models.py
+++ apps/adopcion/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombre = models.CharField(max_length=10)
-    apellido = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
     edad = models.IntegerField()
     telefono = models.CharField(max_length=10)
-    email = models.CharField(max_length=10)
-    domicilio = models.CharField(max_length=10)
+    email = models.CharField(max_length=20)
+    domicilio = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
