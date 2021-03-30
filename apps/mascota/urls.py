7a7b9295e124d1b84ecd515e92dbed04db27d06f
Danie Gallina <danieljgallina@gmail.com>
2021-03-30 17:00:15 -0600

    Agregado el Ver masctoas

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.mascota.views import index, registroMascota
+from apps.mascota.views import index, registroMascota, listaMascota
 
 urlpatterns = [
     path('mascotas', index),
     path('registro', registroMascota),
+    path('ver', listaMascota),
 ]
--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from apps.mascota.form import mascotaForm
 
+from apps.mascota.models import Mascota
+
 # Create your views here.
 def index(request):
     return render(request, 'mascota/index.html')
@@ -11,8 +13,13 @@ def registroMascota(request):
         form = mascotaForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('/zmascotas')
+        return redirect('/mascotas')
     else:
         form = mascotaForm()
     
     return render(request, 'mascota/mascota_form.html', {'form': form})
+
+def listaMascota(request):
+    mascota = Mascota.objects.all()
+    contexto = {'mascota': mascota}
+    return render(request, 'mascota/mascota_list.html', contexto)
